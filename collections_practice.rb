ecdbb6b8e2f0f8af262dedc9ffda216af5b3a5fe
PepperMcLean <jackmcl206@gmail.com>
2021-02-12 16:58:26 -0600

    commit tree 888e2f4717d5d105b6e45875121749909d408e2b

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
 
+def sort_array_asc(array)
+    array.sort 
+end
+
+def sort_array_desc(array)
+    array.sort.reverse 
+end
+
+def sort_array_char_count(array)
+    array.sort do |left, right|
+        left.length <=> right.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+    array[index], array[destination_index] = array[destination_index], array[index]
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |word|
+        word[2] = "$"
+    end
+    array
+end
+
+def find_a(array)
+    array.select {|word| word.start_with? "a"}
+end
+
+def sum_array(array)
+    num = 0
+    array.each do |integer|
+        num += integer
+    end
+    return num
+end
+
+def add_s(array)
+    array.each_with_index do |word, index|
+        if index != 1
+            array[index] += "s"
+        end
+    end
+end
